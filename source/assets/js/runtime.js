68c9e446468643dd0c304e61db760d24222cf60f
Frankfurtlin <linshujing2018@gmail.com>
2026-03-09 00:12:57 +0800

    feat: add bottom

--- source/assets/js/runtime.js
+++ source/assets/js/runtime.js
@@ -19,7 +19,14 @@ function createtime() {
         c = Math.round(d);
     1 == String(c).length && (c = "0" + c);
     let g = "";
-    g = `<span class='textTip'> <br> <b>本站居然运行了 ${o} 天</span><span id='runtime'> ${i} 小时 ${b} 分 ${c} 秒 </b></span> <i id="heartbeat" class='fas fa-heartbeat' style='color:red'></i> <br> <b>旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</b> <br> <b><font size=2px>再看看那个光点，它就在这里，这是家园，这是我们 ——— 你所爱的每一个人，你认识的一个人，你听说过的每一个人，曾经有过的每一个人，都在它上面度过他们的一生✨</font></b> `,
+    g = `<span class='textTip'> <br> 
+    <b>本站居然运行了 ${o} 天</span><span id='runtime'> ${i} 小时 ${b} 分 ${c} 秒 </b>
+    </span> 
+    <i id="heartbeat" class='fas fa-heartbeat' style='color:red'></i>
+     <br> 
+     <b>旅行者 1 号当前距离地球 ${t} 千米，约为 ${a} 个天文单位 🚀</b> 
+     <br>
+      <b><font size=2px>再看看那个光点，它就在这里，这是家园，这是我们 ——— 你所爱的每一个人，你认识的一个人，你听说过的每一个人，曾经有过的每一个人，都在它上面度过他们的一生✨</font></b> `,
         document.getElementById("workboard") && (document.getElementById("workboard").innerHTML = g)
 }
 setInterval((() => {
